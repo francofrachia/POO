322c8817b9bbeb1eb5f948f22f4aad7f8634c912
francofrachia <152249427+francofrachia@users.noreply.github.com>
2024-04-24 21:29:29 -0300

    ejercicio 2 java poo
    fghfgh

---
+++ ej2.java
@@ -0,0 +1,37 @@
+package POO.tp2;
+
+import java.util.Scanner;
+
+public class asd {
+            public static void main(String[] args) {
+            Scanner scanner = new Scanner (System.in);
+            char continuar = 's';
+
+        do{
+            System.out.println("ingrese el primer nùmero");
+            double num1 = scanner.nextDouble();
+
+            System.out.println("Ingrese el segundo Nùmero");
+            double num2 = scanner.nextDouble();
+
+            System.out.println("Ingrese el segundo nùmero");
+            double num3 = scanner.nextDouble();
+
+            if (num1>0) {
+                double producto = num2 * num3;
+                System.out.println("El producto de los dos ùltimos nùmeros es: " + producto);
+            }
+                else {
+                    double suma = num2 + num3;
+                    System.out.println("La suma de los dos ùltimos nùmeros es: " + suma);
+                }
+            System.out.println("¿Desea realizar otra operaciòn? (s/n) ");
+            continuar = scanner.next().charAt(0);
+        } while (continuar == 's' || continuar == 'S');     
+        {
+            scanner.close();
+        }
+        
+    }
+
+}
