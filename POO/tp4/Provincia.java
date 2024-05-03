1963036cedc01a3ee107e26f188e23310faa1d50
francofrachia <152249427+francofrachia@users.noreply.github.com>
2024-05-03 19:32:06 -0300

    clase provincia
    agrugue classe prov

---
+++ POO/tp4/Provincia.java
@@ -0,0 +1,33 @@
+package POO.tp4;
+
+public class Provincia {
+
+    public static void main(String[] args) {
+        
+    }
+
+    private String nombre;
+    private String pais;
+
+    public Provincia(String nombre, String pais){
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+}
